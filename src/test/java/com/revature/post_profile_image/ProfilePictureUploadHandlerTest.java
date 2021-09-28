94bbe35a076e0a8f45032c80745fd42292296c0f
heraldOfMechanus <jccallahan11@gmail.com>
2021-09-27 19:20:51 -0500

    Wrote tests for the ProfilePictureUploadHandler.

--- src/test/java/com/revature/post_profile_image/ProfilePictureUploadHandlerTest.java
+++ src/test/java/com/revature/post_profile_image/ProfilePictureUploadHandlerTest.java
@@ -1,48 +1,105 @@
 package com.revature.post_profile_image;
 
+import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.revature.post_profile_image.stubs.TestLogger;
 import org.apache.commons.io.IOUtils;
-import org.mockito.internal.util.io.IOUtil;
-
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Collections;
-import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class ProfilePictureUploadHandlerTest {
 
+    static TestLogger testLogger;
+
     ProfilePictureUploadHandler sut;
+    Context mockContext;
 
-
+    @org.junit.jupiter.api.BeforeAll
+    public static void beforeAll() { testLogger = new TestLogger(); }
 
     @org.junit.jupiter.api.BeforeEach
     void setUp() {
         sut = new ProfilePictureUploadHandler();
+
+        mockContext = mock(Context.class);
+        when(mockContext.getLogger()).thenReturn(testLogger);
     }
 
     @org.junit.jupiter.api.AfterEach
     void tearDown() {
         sut = null;
+        mockContext = null;
     }
 
     @org.junit.jupiter.api.Test
-    void handleRequest_IfGiven_ValidBase64Image() throws IOException {
+    void handleRequest_IfGiven_ValidBase64Image_Return500_S3ClientNotFound() throws IOException {
         // Arrange
         FileInputStream bigChungus = new FileInputStream("src/test/resources/big-base-64.txt");
         String base64 = IOUtils.toString(bigChungus);
 
-        APIGatewayProxyRequestEvent requestEvent = new APIGatewayProxyRequestEvent();
-        requestEvent.withBody(base64);
-        requestEvent.withHeaders(Collections.singletonMap("Content-Type", "base64"));
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "text/plain; charset=ISO-5589-1");
+        headers.put("Content-transfer-encoding", "base64");
 
+        APIGatewayProxyRequestEvent mockRequestEvent = new APIGatewayProxyRequestEvent();
+        mockRequestEvent.withPath("/users/images");
+        mockRequestEvent.withHttpMethod("POST");
+        mockRequestEvent.withBody(base64);
+        mockRequestEvent.setIsBase64Encoded(true);
+        mockRequestEvent.setHeaders(headers);
+        mockRequestEvent.withQueryStringParameters(null);
 
         // Act
+        APIGatewayProxyResponseEvent responseEvent = sut.handleRequest(mockRequestEvent, mockContext);
 
         // Assert
+        assertEquals(500, responseEvent.getStatusCode());
+    }
 
+    @org.junit.jupiter.api.Test
+    void handleRequest_Return400_IfGiven_ValidBase64Image_withNoContentTypeHeader() throws IOException {
+        // Arrange
+        FileInputStream bigChungus = new FileInputStream("src/test/resources/big-base-64.txt");
+        String base64 = IOUtils.toString(bigChungus);
+
+        APIGatewayProxyRequestEvent mockRequestEvent = new APIGatewayProxyRequestEvent();
+        mockRequestEvent.withPath("/users/images");
+        mockRequestEvent.withHttpMethod("POST");
+        mockRequestEvent.withBody(base64);
+        mockRequestEvent.setIsBase64Encoded(true);
+        mockRequestEvent.withQueryStringParameters(null);
+
+        // Act
+        APIGatewayProxyResponseEvent responseEvent = sut.handleRequest(mockRequestEvent, mockContext);
+
+        // Assert
+        assertEquals(400, responseEvent.getStatusCode());
+    }
+
+    @org.junit.jupiter.api.Test
+    void handleRequest_Return400_IfGiven_InvalidContent() throws IOException {
+        // Arrange
+        FileInputStream bigChungus = new FileInputStream("src/test/resources/big-base-64.txt");
+        String base64 = IOUtils.toString(bigChungus);
+
+        APIGatewayProxyRequestEvent mockRequestEvent = new APIGatewayProxyRequestEvent();
+        mockRequestEvent.withPath("/users/images");
+        mockRequestEvent.withHttpMethod("POST");
+        mockRequestEvent.withBody(base64);
+        mockRequestEvent.setIsBase64Encoded(false);
+        mockRequestEvent.withQueryStringParameters(null);
+
+        // Act
+        APIGatewayProxyResponseEvent responseEvent = sut.handleRequest(mockRequestEvent, mockContext);
+
+        // Assert
+        assertEquals(400, responseEvent.getStatusCode());
     }
 }
