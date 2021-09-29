f31c020fdcb2d69522b49bcb09d68ccae721f8a7
heraldOfMechanus <jccallahan11@gmail.com>
2021-09-29 12:02:45 -0500

    Fixed the tests such that queryStringParameters are properly grabbed.

--- src/test/java/com/revature/post_profile_image/ProfilePictureUploadHandlerTest.java
+++ src/test/java/com/revature/post_profile_image/ProfilePictureUploadHandlerTest.java
@@ -73,7 +73,7 @@ class ProfilePictureUploadHandlerTest {
         APIGatewayProxyRequestEvent mockRequestEvent = new APIGatewayProxyRequestEvent();
         mockRequestEvent.withPath("/users/images");
         mockRequestEvent.withHttpMethod("POST");
-        mockRequestEvent.withQueryStringParameters(Collections.singletonMap("user_id", "valid"));
+        mockRequestEvent.withQueryStringParameters(Collections.singletonMap("user_id", "B1GCHUNGU5"));
         mockRequestEvent.withBody(base64);
         mockRequestEvent.setIsBase64Encoded(true);
         mockRequestEvent.setHeaders(headers);
@@ -97,7 +97,6 @@ class ProfilePictureUploadHandlerTest {
         mockRequestEvent.withQueryStringParameters(Collections.singletonMap("user_id", "valid"));
         mockRequestEvent.withBody(base64);
         mockRequestEvent.setIsBase64Encoded(true);
-        mockRequestEvent.withQueryStringParameters(Collections.singletonMap("user_id", "valid"));
 
         // Act
         APIGatewayProxyResponseEvent responseEvent = sut.handleRequest(mockRequestEvent, mockContext);
