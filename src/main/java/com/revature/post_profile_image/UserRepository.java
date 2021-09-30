50c26235e5412862f287726c65f781a42f251f84
heraldOfMechanus <jccallahan11@gmail.com>
2021-09-29 20:00:55 -0500

    Created ResourceNotFoundException

---
+++ src/main/java/com/revature/post_profile_image/UserRepository.java
@@ -0,0 +1,104 @@
+package com.revature.post_profile_image;
+
+import com.revature.post_profile_image.exceptions.ResourceNotFoundException;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
+import software.amazon.awssdk.enhanced.dynamodb.Expression;
+import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey;
+import software.amazon.awssdk.enhanced.dynamodb.model.ScanEnhancedRequest;
+import software.amazon.awssdk.http.apache.ApacheHttpClient;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
+
+import java.util.List;
+
+public class UserRepository {
+
+    private DynamoDbTable<User> userTable;
+
+    public UserRepository(DynamoDbTable<User> userTable) {
+        this.userTable = userTable;
+    }
+
+    public UserRepository() {
+        DynamoDbClient db = DynamoDbClient.builder().httpClient(ApacheHttpClient.create()).build();
+        DynamoDbEnhancedClient dbClient = DynamoDbEnhancedClient.builder().dynamoDbClient(db).build();
+        userTable = dbClient.table("Users", TableSchema.fromBean(User.class));
+    }
+
+    public User findUserById(String id) {
+        AttributeValue val = AttributeValue.builder().s(id).build();
+        // Make an expression where '#a' and ':b' are variables. Assign those variables to a string and value, a hashset.
+        Expression filter = Expression.builder().expression("#a = :b").putExpressionName("#a", "id").putExpressionValue(":b", val).build();
+        ScanEnhancedRequest request = ScanEnhancedRequest.builder().filterExpression(filter).build();
+
+        User user = userTable.scan(request).stream()
+                .findFirst()
+                .orElseThrow(ResourceNotFoundException::new)
+                .items().get(0);
+
+        System.out.println("User found: " + user);
+        return user;
+    }
+}
+
+/**
+ * The User POJO is necessary for storing the data received from DynamoDB.
+ * It is very much a Data Transfer Object.
+ */
+@Data
+@Builder
+@DynamoDbBean
+class User {
+    private String id;
+    private String username;
+    private List<SetDocument> favorite_sets;
+    private List<SetDocument> created_sets;
+    private String profile_picture;
+    private int points;
+    private int wins;
+    private int losses;
+    private String registration_date;
+    private List<String> gameRecords;
+
+    @DynamoDbPartitionKey
+    public String getId() {
+        return id;
+    }
+}
+
+/**
+ * SetDocument is necessary because the User returned from DynamoDB has a
+ * list of SetDocuments attached to them describing the user's favorite cards
+ * and their created list of cards. This is what we are altering.
+ */
+@Data
+@AllArgsConstructor
+class SetDocument {
+    private String id;
+    private String set_name;
+    private List<Tags> tags;
+    private String author;
+    private boolean isPublic;
+    private int views;
+    private int plays;
+    private int studies;
+    private int favorites;
+}
+
+/**
+ * Tags is a necessary pojo for persisting data from the DynamoDB to access
+ * the users from the database. Without the Tags, it is impossible to handle
+ * user data safely.
+ */
+@Data
+@AllArgsConstructor
+class Tags {
+    private String name;
+    private String color;
+}
